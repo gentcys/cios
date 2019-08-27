c6ec787875c981993fb57491ac776b85203bfc00
gentcys <iamchencong@gmail.com>
2019-08-27 16:31:42 +0800

    Use Bulma instead of Materialize as CSS framework

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-
 import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
